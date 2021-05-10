f78ee6270da04a7559f1850f3da2f0657dbb508c
Vynkode <kevinmuqu@me.com>
2021-05-10 16:12:57 +0200

    Socket testing

---
+++ testwebsocket/web/src/components/Progress/Progress.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect, useRef } from 'react';
+import io from 'socket.io-client';
+
+const Progress = () => {
+  const [progress, setProgress] = useState(0);
+
+  const socketRef = useRef();
+
+  useEffect(() => {
+    socketRef.current = io.connect('http://localhost:4000');
+    socketRef.current.on(`progress${level}`, () =>
+      setProgress(prevState => prevState + 20)
+    );
+    socketRef.current.on('disconnect', () => {
+      console.log(socketRef.current.connected);
+    });
+    return function disconnect() {
+      socketRef.current.disconnect();
+    };
+  }, []);
+
+  return <div className={`progress ${progress > 15 ? 'active' : ''}`}></div>;
+};
+
+export default Progress;
--- testwebsocket/web/src/components/TrainLevel/TrainLevel.js
+++ testwebsocket/web/src/components/TrainLevel/TrainLevel.js
@@ -1,20 +1,20 @@
 import React, { useState, useEffect, useRef } from 'react';
 import io from 'socket.io-client';
 
+//CSS
 import './TrainLevel.css';
+
+//Components
+import Progress from '../Progress/Progress';
 
 const TrainLevel = ({ socket, level }) => {
   const [active, setActive] = useState(false);
-  const [progress, setProgress] = useState(0);
 
   const socketRef = useRef();
 
   useEffect(() => {
     socketRef.current = io.connect('http://localhost:4000');
     socketRef.current.on(`train${level}Begin`, () => setActive(true));
-    socketRef.current.on(`progress${level}`, () =>
-      setProgress(prevState => prevState + 20)
-    );
     socketRef.current.on('disconnect', () => {
       console.log(socketRef.current.connected);
     });
@@ -23,13 +23,10 @@ const TrainLevel = ({ socket, level }) => {
     };
   }, []);
   console.log(`Render Train ${level}`);
+
   return (
     <div className={`train train${level} ${active > 0 ? 'active' : ''}`}>
-      <div className={`progress ${progress > 15 ? 'active' : ''}`}></div>
-      <div className={`progress ${progress > 35 ? 'active' : ''}`}></div>
-      <div className={`progress ${progress > 55 ? 'active' : ''}`}></div>
-      <div className={`progress ${progress > 75 ? 'active' : ''}`}></div>
-      <div className={`progress ${progress > 95 ? 'active' : ''}`}></div>
+      <Progress />
     </div>
   );
 };
